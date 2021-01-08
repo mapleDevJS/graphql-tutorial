4bdf0e1e7a018ce912d7eae8875a627ad0e098d2
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-01-08 10:36:25 -0500

    feat: Perform a GraphQL request
    import gql template tag, get an access token for the API, use an Apollo Link to authenticate

--- src/index.js
+++ src/index.js
@@ -4,11 +4,28 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
+import { setContext } from 'apollo-link-context'
+import gql from 'graphql-tag'
 
 import App from './App'
 
+const httpLink = createHttpLink({ uri: 'https://api.github.com/graphql' })
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.REACT_APP_API_TOKEN
+
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`
+    }
+  }
+})
+
+const link = authLink.concat(httpLink)
+
 const client = new ApolloClient({
-  link: createHttpLink({ uri: 'https://api.github.com/graphql' }),
+  link: link,
   cache: new InMemoryCache()
 })
 
@@ -18,3 +35,26 @@ ReactDOM.render(
   </ApolloProvider>,
   document.getElementById('root')
 )
+
+const POPULAR_REPOSITORIES_LIST = gql`
+{
+  search(query: "stars:>50000", type: REPOSITORY, first: 10) {
+    repositoryCount
+    edges {
+      node {
+        ... on Repository {
+          name
+          owner {
+            login
+          }
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+client.query({ query: POPULAR_REPOSITORIES_LIST }).then(console.log)
