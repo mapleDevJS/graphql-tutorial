0cf0bb51cad86ead626e29372c8cb4ca77c9befb
Alexey Ivanov <alexey.ivanov.js@gmail.com>
2024-06-21 11:01:48 -0400

    Add index.html and refactor JavaScript files to JSX
    A new index.html file was created and set up for the Vite + React environment. Also, the existing JavaScript files were renamed and refactored to JSX files to better align with React standards. Changes were made to allow loading environment variables using Vite's import.meta.env system.

--- src/main.jsx
+++ src/main.jsx
@@ -6,35 +6,37 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
 import { setContext } from 'apollo-link-context'
 import gql from 'graphql-tag'
-
-import App from './App'
+import App from './App.jsx'
+import './index.css'
 
 const httpLink = createHttpLink({ uri: 'https://api.github.com/graphql' })
 
 const authLink = setContext((_, { headers }) => {
-  const token = process.env.REACT_APP_API_TOKEN
+    const token = import.meta.env.VITE_GITHUB_API_TOKEN
+    console.log('token:', token);
 
-  return {
-    headers: {
-      ...headers,
-      authorization: `Bearer ${token}`
+    return {
+        headers: {
+            ...headers,
+            authorization: `Bearer ${token}`
+        }
     }
-  }
 })
 
 const link = authLink.concat(httpLink)
 
 const client = new ApolloClient({
-  link: link,
-  cache: new InMemoryCache()
+    link: link,
+    cache: new InMemoryCache()
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
 )
+
 
 const POPULAR_REPOSITORIES_LIST = gql`
 {
