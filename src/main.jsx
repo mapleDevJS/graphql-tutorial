22fb1a080b5ffeb020d753d30242663f6461fef7
Alexey Ivanov <alexey.ivanov.js@gmail.com>
2024-06-21 11:16:55 -0400

    Add comments to main.jsx for better code understanding
    The commit enhances code readability by adding comprehensive comments to the main.jsx file. These comments explain the purpose and functionality of the GraphQL URI's and methods, http links, and authentication links. It also gives clear descriptions for the client object and variables related to repository searching.

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,30 @@ import gql from 'graphql-tag';
 import App from './App';
 import './index.css';
 
+/**
+ * The GraphQL URI for the GitHub API.
+ *
+ * The GRAPHQL_URI variable is a string that contains the URL for the GraphQL endpoint of the GitHub API.
+ * By using this URI, clients can send GraphQL queries and mutations to interact with the GitHub platform.
+ *
+ * The value of GRAPHQL_URI is 'https://api.github.com/graphql'.
+ *
+ * @type {string}
+ */
 const GRAPHQL_URI = 'https://api.github.com/graphql';
+/**
+ * The key used to access the authorization header in a request.
+ *
+ * @type {string}
+ */
 const AUTH_HEADER_KEY = 'authorization';
 
+/**
+ * Sets the authorization token in the headers by wrapping the Apollo Client's setContext function.
+ *
+ * @function createAuthLink
+ * @returns {Object} - The headers object with the authorization token.
+ */
 const createAuthLink = () => setContext((_, { headers }) => {
     const token = import.meta.env.VITE_GITHUB_API_TOKEN;
     console.log('token:', token);
@@ -22,15 +43,55 @@ const createAuthLink = () => setContext((_, { headers }) => {
     };
 });
 
+/**
+ * The `httpLink` variable represents an HTTP link for connecting to a GraphQL server.
+ * It is created using the `createHttpLink` function and configured with a URI (Uniform Resource Identifier)
+ * pointing to the GraphQL endpoint.
+ *
+ * @type {object}
+ *
+ * @property {function} request - The function used to execute requests made using the HTTP link.
+ * @property {function} options - The options for configuring the HTTP link behavior, such as the URI.
+ *
+ * @since - Since the creation of this variable, there have been no changes.
+ */
 const httpLink = createHttpLink({ uri: GRAPHQL_URI });
+/**
+ * This variable represents an authentication link that is created through the function `createAuthLink()`.
+ * The `createAuthLink()` function returns an authentication link which can be used for user authentication.
+ *
+ * @type {string}
+ */
 const authLink = createAuthLink();
+/**
+ * Represents a composed Apollo Link that combines an authentication link and an HTTP link.
+ * The `apolloLink` variable is created by concatenating the `authLink` and `httpLink` links together.
+ *
+ * @type {ApolloLink}
+ * @memberof module:myApp
+ */
 const apolloLink = authLink.concat(httpLink);
 
+/**
+ * Represents a client object that interacts with the Apollo server.
+ * @class
+ * @constructor
+ * @param {Object} options - The options to customize the client behavior.
+ * @param {ApolloLink} options.link - The link object to handle the network requests.
+ * @param {InMemoryCache} options.cache - The cache object to store the query results.
+ */
 const client = new ApolloClient({
     link: apolloLink,
     cache: new InMemoryCache(),
 });
 
+/**
+ * A GraphQL query string that retrieves a list of popular repositories.
+ * The query searches for repositories with more than 50,000 stars and
+ * returns the top 10 results.
+ *
+ * @type {string}
+ */
 const POPULAR_REPOSITORIES_LIST = gql`
 {
   search(query: "stars:>50000", type: REPOSITORY, first: 10) {
